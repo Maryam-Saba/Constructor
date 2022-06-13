abb3371079471aa946fac2ea0c5eaa783ed1591a
Maryam-Saba <105993857+Maryam-Saba@users.noreply.github.com>
2022-06-14 00:50:06 +0500

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,20 @@
+class Addition
+{
+int x;
+int y;
+
+Addition()
+{
+x=50;
+y=60;
+
+}
+}
+public class Constructor
+{
+public static void main(String args[])
+{
+Addition P= new Addition();
+System.out.println(P.x + P.y);
+}
+}
